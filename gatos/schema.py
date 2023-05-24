0805a9add2814e863dccb0a952c8e552fa898c15
Jazael Galindo <115960860+JazaeloG@users.noreply.github.com>
2023-05-23 23:08:06 -0600

    mutations

--- gatos/schema.py
+++ gatos/schema.py
@@ -16,3 +16,43 @@ class Query(graphene.ObjectType):
     
 
 
+class CreateGato(graphene.Mutation):
+    id = graphene.Int()
+    name = graphene.String()
+    image = graphene.String()
+    ataque = graphene.Int()
+    defensa = graphene.Int()
+    tipo = graphene.String()
+    counter = graphene.String()
+    strong = graphene.String()
+
+    #2
+    class Arguments:
+        name = graphene.String()
+        image = graphene.String()
+        ataque = graphene.Int()
+        defensa = graphene.Int()
+        tipo = graphene.String()
+        counter = graphene.String()
+        strong = graphene.String()
+
+    #3
+    def mutate(self, info, name, image,ataque,defensa,counter,strong,tipo):
+        gato = Gato(name=name, image=image,ataque=ataque,strong=strong,counter=counter,defensa=defensa,tipo=tipo)
+        gato.save()
+
+        return CreateGato(
+            id =gato.id,
+            name=gato.name,
+            tipo=gato.tipo,
+            ataque=gato.ataque,
+            counter=gato.counter,
+            defensa=gato.defensa,
+            image=gato.image,
+            strong=gato.strong
+        )
+
+
+#4
+class Mutation(graphene.ObjectType):
+    create_gato = CreateGato.Field()
--- gatosAPI/schema.py
+++ gatosAPI/schema.py
@@ -7,6 +7,9 @@ class Query(schema.Query, graphene.ObjectType):
     pass
 
 
+class Mutation(schema.Mutation, graphene.ObjectType):
+    pass
 
 
-schema = graphene.Schema(query=Query)
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
